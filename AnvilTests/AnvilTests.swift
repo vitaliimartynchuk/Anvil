dcb6d039af4e941df13610e5fb22d0ac0f649c43
vitaliimartynchuk <vitaliimartun@gmail.com>
2026-06-27 20:47:02 +0200

    Initial Commit

---
+++ AnvilTests/AnvilTests.swift
@@ -0,0 +1,19 @@
+//
+//  AnvilTests.swift
+//  AnvilTests
+//
+//  Created by Vitalii Martynchuk on 27.06.2026.
+//
+
+import Testing
+@testable import Anvil
+
+struct AnvilTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
